26ac0543040ae4847df90fbf5ea892d5430d254c
BuildTools <unconfigured@null.spigotmc.org>
2021-07-14 09:01:49 +0900

    first

---
+++ app/src/main/java/tk/mjsv/project30519/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.mjsv.project30519;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
